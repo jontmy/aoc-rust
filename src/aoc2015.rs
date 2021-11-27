a2c494e3429df881f39bb0ba47586258d800c23c
jontmy00 <jontmy001@gmail.com>
2021-11-27 17:33:19 +0800

    Solve 2015.03.1

--- src/aoc2015.rs
+++ src/aoc2015.rs
@@ -3,6 +3,7 @@ use crate::utils::io::read;
 
 mod day01;
 mod day02;
+mod day03;
 
 pub fn run() {
     let input = read(1, 2015);
@@ -14,4 +15,9 @@ pub fn run() {
     io::write(2, 2015,
               day02::solve_part_one(&input),
               day02::solve_part_two(&input));
+
+    let input = read(3, 2015);
+    io::write(3, 2015,
+              day03::solve_part_one(&input),
+              day03::solve_part_two(&input));
 }
--- src/aoc2015/day03.rs
+++ src/aoc2015/day03.rs
@@ -1,25 +1,72 @@
+use std::collections::HashMap;
+
 pub fn solve_part_one(input: &String) -> i32 {
-    0
+    // Track the number of visits to each house.
+    let mut frequencies = input.trim()
+        .chars()
+        .scan(House{ x: 0, y: 0 }, |state, c: char| {
+            *state = state.next(c);
+            Some(*state)
+        })
+        .fold(HashMap::new(), |mut freqs, value| -> HashMap<House, i32>{
+            *freqs.entry(value).or_insert(0) += 1;
+            freqs
+        });
+
+    // Add the visit to the first house at (0, 0).
+    *frequencies.entry(House {x: 0, y: 0}).or_insert(0) += 1;
+
+    // Return the number of houses visited.
+    frequencies.len() as i32
 }
 
 pub fn solve_part_two(input: &String) -> i32 {
     0
 }
 
+#[derive(Hash, Copy, Clone)]
+struct House {
+    x: i32,
+    y: i32
+}
+
+impl Eq for House { }
+impl PartialEq<Self> for House {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
+
+impl House {
+    fn next(&self, c: char) -> House {
+        match c {
+            '<' => House{ x: self.x - 1, y: self.y },
+            '>' => House{ x: self.x + 1, y: self.y },
+            '^' => House{ x: self.x, y: self.y + 1 },
+            'v' => House{ x: self.x, y: self.y - 1 },
+            _ => panic!("Unmatched character {}", c)
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::aoc2015::day01::{solve_part_one, solve_part_two};
+    use crate::aoc2015::day03::{solve_part_one, solve_part_two};
     use rstest::rstest;
 
     #[rstest]
-    #[case("(())", 0)]
+    #[case(">", 2)]
+    #[case("^>v<", 4)]
+    #[case("^v^v^v^v^v", 2)]
     fn test_part_one(#[case] input: String, #[case] expected: i32) {
         assert_eq!(expected, solve_part_one(&input))
     }
 
+    /*
     #[rstest]
     #[case(")", 1)]
     fn test_part_two(#[case] input: String, #[case] expected: i32) {
         assert_eq!(expected, solve_part_two(&input))
     }
+     */
 }
