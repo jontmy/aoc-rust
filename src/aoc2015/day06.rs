3bd428accd2258b6ece66915c747ed92d97ea145
jontmy00 <jontmy001@gmail.com>
2021-11-29 13:03:56 +0800

    Solve 2015.06.2

--- src/aoc2015/day06.rs
+++ src/aoc2015/day06.rs
@@ -1,3 +1,4 @@
+use std::cmp::{max, min};
 use regex::{Regex};
 
 pub fn solve_part_one(input: &String) -> i32 {
@@ -6,7 +7,7 @@ pub fn solve_part_one(input: &String) -> i32 {
     let re = Regex::new(r"(?m)^(turn off|turn on|toggle) (\d+),(\d+) through (\d+),(\d+)$").unwrap();
     let captures = re.captures_iter(input)
         .for_each(|capture| {
-            grid.act(
+            grid.act_one(
                 capture.get(1).map_or("", |m| m.as_str()),
                 capture.get(2).map_or(0, |m| m.as_str().parse().unwrap()),
                 capture.get(3).map_or(0, |m| m.as_str().parse().unwrap()),
@@ -20,7 +21,7 @@ pub fn solve_part_one(input: &String) -> i32 {
 
 // The 1000 x 1000 grid has only 1 million entries, so a 2D array is fine, though not technically ideal.
 struct Grid {
-    grid: [[u8; 1000]; 1000]
+    grid: [[i8; 1000]; 1000]
 }
 
 impl Grid {
@@ -30,13 +31,27 @@ impl Grid {
         }
     }
 
-    fn act(&mut self, action: &str, x_start: usize, y_start: usize, x_end: usize, y_end: usize) {
+    fn act_one(&mut self, action: &str, x_start: usize, y_start: usize, x_end: usize, y_end: usize) {
         for x in x_start..=x_end {
             for y in y_start..=y_end {
                 self.grid[x][y] = match action {
                     "turn on" => 1,
                     "turn off" => 0,
                     "toggle" => if self.grid[x][y] == 0 {1} else {0},
+                    _ => panic!()
+                };
+            }
+        }
+    }
+
+    fn act_two(&mut self, action: &str, x_start: usize, y_start: usize, x_end: usize, y_end: usize) {
+        for x in x_start..=x_end {
+            for y in y_start..=y_end {
+                let val = self.grid[x][y];
+                self.grid[x][y] = match action {
+                    "turn on" => min(100, val + 1),
+                    "turn off" => max(0, val - 1),
+                    "toggle" => min(100, val + 2),
                     _ => panic!()
                 };
             }
@@ -51,10 +66,31 @@ impl Grid {
         })
         .sum()
     }
+
+    fn count_total_brightness(&self) -> i32 {
+        self.grid.iter()
+            .flat_map(|row| *row)
+            .map(i32::from)
+            .sum()
+    }
 }
 
 pub fn solve_part_two(input: &String) -> i32 {
-    0
+    let mut grid = Grid::new();
+
+    let re = Regex::new(r"(?m)^(turn off|turn on|toggle) (\d+),(\d+) through (\d+),(\d+)$").unwrap();
+    let captures = re.captures_iter(input)
+        .for_each(|capture| {
+            grid.act_two(
+                capture.get(1).map_or("", |m| m.as_str()),
+                capture.get(2).map_or(0, |m| m.as_str().parse().unwrap()),
+                capture.get(3).map_or(0, |m| m.as_str().parse().unwrap()),
+                capture.get(4).map_or(0, |m| m.as_str().parse().unwrap()),
+                capture.get(5).map_or(0, |m| m.as_str().parse().unwrap()),
+            )
+        });
+
+    grid.count_total_brightness()
 }
 
 #[cfg(test)]
@@ -63,14 +99,15 @@ mod tests {
     use rstest::rstest;
 
     #[rstest]
-    #[case("turn on 0,0 through 999,999", 1_000_000)]
     #[case("toggle 0,0 through 999,0", 1000)]
+    #[case("turn on 0,0 through 999,999", 1_000_000)]
     fn test_part_one(#[case] input: String, #[case] expected: i32) {
         assert_eq!(expected, solve_part_one(&input))
     }
 
     #[rstest]
-    #[case("^v", 3)]
+    #[case("turn on 0,0 through 0,0", 1)]
+    #[case("toggle 0,0 through 999,999", 2_000_000)]
     fn test_part_two(#[case] input: String, #[case] expected: i32) {
         assert_eq!(expected, solve_part_two(&input))
     }
