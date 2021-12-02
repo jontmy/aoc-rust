9beffccd8850e3e0503e7966be3c60e464468410
jontmy00 <jontmy001@gmail.com>
2021-12-02 11:53:13 +0800

    Prepare for 2021.02

--- src/aoc2021.rs
+++ src/aoc2021.rs
@@ -2,10 +2,18 @@ use crate::utils::io;
 use crate::utils::io::read;
 
 mod day01;
+mod day02;
 
 pub fn run() {
+    /*
     let input = read(1, 2021);
     io::write(1, 2021,
               day01::solve_part_one(&input),
               day01::solve_part_two(&input));
+    */
+
+    let input = read(2, 2015);
+    io::write(2, 2015,
+              day02::solve_part_one(&input),
+              day02::solve_part_two(&input));
 }
---
+++ src/aoc2021/day02.rs
@@ -0,0 +1,38 @@
+use itertools::Itertools;
+
+pub fn solve_part_one(input: &String) -> i32 {
+    input.lines()
+        .map(|s| s.parse().unwrap());
+
+    0
+}
+
+pub fn solve_part_two(input: &String) -> i32 {
+    input.lines()
+        .map(|s| s.parse().unwrap());
+
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+
+    use super::{solve_part_one, solve_part_two};
+
+    #[rstest]
+    #[case(indoc::indoc ! {"
+
+    "}.to_string(), 0)]
+    fn test_part_one(#[case] input: String, #[case] expected: usize) {
+        assert_eq!(expected, solve_part_one(&input))
+    }
+
+    #[rstest]
+    #[case(indoc::indoc ! {"
+
+    "}.to_string(), 0)]
+    fn test_part_two(#[case] input: String, #[case] expected: usize) {
+        assert_eq!(expected, solve_part_two(&input))
+    }
+}
