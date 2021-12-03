1cfa1ceab8f9b80de65ff2e6d565477a7ec9004b
jontmy00 <jontmy001@gmail.com>
2021-12-03 16:27:45 +0800

    Refactor 2021.03

--- src/aoc2021/day03.rs
+++ src/aoc2021/day03.rs
@@ -1,45 +1,31 @@
-
 use itertools::Itertools;
 
 pub fn solve_part_one(input: &String) -> i32 {
-    let bins = input.lines()
-        .map(|s| s.to_string())
-        .collect_vec();
+    let (gamma, epsilon): (Vec<char>, Vec<char>) = frequencies(input).iter()
+        .map(|(zeroes, ones)| if zeroes > ones { ('0', '1') } else { ('1', '0') })
+        .unzip();
 
-    let (gamma, epsilon) = gamma_epsilon(&bins.iter().collect_vec());
-    let gamma = isize::from_str_radix(gamma.as_str(), 2).unwrap();
-    let epsilon = isize::from_str_radix(epsilon.as_str(), 2).unwrap();
+    let gamma = i32::from_str_radix(&*gamma.iter().collect::<String>(), 2).unwrap();
+    let epsilon = i32::from_str_radix(&*epsilon.iter().collect::<String>(), 2).unwrap();
 
-    (gamma * epsilon).try_into().unwrap()
+    gamma * epsilon
 }
 
 // Converts a vector of binary strings into a vector of frequency('0', '1') tuples at each index.
-fn frequencies(bins: &Vec<&String>) -> Vec<(i32, i32)> {
-    let mut freqs = vec!((0, 0); bins[0].len());
-    for bin in bins {
-        for (i, c) in bin.chars().enumerate() {
-            match c {
-                '0' => freqs[i].0 += 1,
-                '1' => freqs[i].1 += 1,
-                _ => panic!(),
-            }
-        }
-    };
-    freqs
-}
+fn frequencies(bins: &String) -> Vec<(usize, usize)> {
+    let index_char_map = bins.lines()
+        .flat_map(|bin| bin.chars().enumerate())
+        .into_group_map();
 
-fn gamma_epsilon(bins: &Vec<&String>) -> (String, String) {
-    let (gamma, epsilon): (Vec<_>, Vec<_>) = frequencies(bins).iter()
-        .map(|(zeroes, ones)| {
-            if zeroes > ones {
-                ('0', '1')
-            } else {
-                ('1', '0')
-            }
+    (0..index_char_map.len())
+        .map(|i| {
+            let (zeroes, ones): (Vec<char>, Vec<char>) = index_char_map.get(&i)
+                .unwrap()
+                .iter()
+                .partition(|&&c| c == '0');
+            (zeroes.len(), ones.len())
         })
-        .unzip();
-
-    (gamma.iter().collect::<String>(), epsilon.iter().collect::<String>())
+        .collect_vec()
 }
 
 pub fn solve_part_two(input: &String) -> i32 {
@@ -48,33 +34,58 @@ pub fn solve_part_two(input: &String) -> i32 {
         .collect_vec();
     let len = lines[0].len();
 
-    let mut o2 = lines.iter().collect_vec();
-    for i in 0..len {
-        let (gamma, _) = gamma_epsilon(&o2);
-        o2 = o2.iter()
-            .filter(|&bin| bin[i..=i] == gamma[i..=i])
-            .map(|s| *s)
-            .collect_vec();
-        dbg!(&o2);
-        if o2.len() <= 1 { break }
-    }
+    let initial = (
+        lines.iter().collect_vec(),
+        lines.iter().collect_vec()
+    );
 
-    let mut co2 = lines.iter().collect_vec();
-    for i in 0..len {
-        let (_, epsilon) = gamma_epsilon(&co2);
-        co2 = co2.iter()
-            .filter(|&bin| bin[i..=i] == epsilon[i..=i])
-            .map(|s| *s)
-            .collect_vec();
-        dbg!(epsilon);
-        dbg!(&co2);
-        if co2.len() <= 1 { break }
-    }
+    let (o2, co2) = (0..len).into_iter()
+        .fold(initial, |(o2, co2), i| {
+            let (o2_bit, co2_bit) = bit_criteria(&o2, &co2, i);
 
-    let o2_rating = isize::from_str_radix(o2[0], 2).unwrap();
-    let co2_rating = isize::from_str_radix(co2[0], 2).unwrap();
+            let o2 = match o2.len() {
+                1 => o2,
+                _ => o2.iter()
+                    .filter(|&&bin| bin[i..=i] == o2_bit)
+                    .map(|s| *s)
+                    .collect_vec()
+            };
 
-    (o2_rating * co2_rating).try_into().unwrap()
+            let co2 = match co2.len() {
+                1 => co2,
+                _ => co2.iter()
+                    .filter(|&&bin| bin[i..=i] == co2_bit)
+                    .map(|s| *s)
+                    .collect_vec()
+            };
+
+            (o2, co2)
+        });
+
+    let o2_rating = i32::from_str_radix(o2[0], 2).unwrap();
+    let co2_rating = i32::from_str_radix(co2[0], 2).unwrap();
+
+    o2_rating * co2_rating
+}
+
+fn bit_criteria(o2: &Vec<&String>, co2: &Vec<&String>, index: usize) -> (String, String) {
+    let (zeroes, ones): (Vec<char>, Vec<char>) = o2.iter()
+        .map(|bin| bin.chars()
+            .nth(index)
+            .unwrap())
+        .partition(|&c| c == '0');
+
+    let most_common = if ones.len() >= zeroes.len() { '1' } else { '0' };
+
+    let (zeroes, ones): (Vec<char>, Vec<char>) = co2.iter()
+        .map(|bin| bin.chars()
+            .nth(index)
+            .unwrap())
+        .partition(|&c| c == '0');
+
+    let least_common = if zeroes.len() <= ones.len() { '0' } else { '1' };
+
+    (most_common.to_string(), least_common.to_string())
 }
 
 #[cfg(test)]
