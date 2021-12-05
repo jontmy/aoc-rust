ce3408ee1fb97d90c9a31b7538f33a416cc55f01
jontmy00 <jontmy001@gmail.com>
2021-12-05 14:25:28 +0800

    Refactor 2021.05.1

--- src/aoc2021/day05.rs
+++ src/aoc2021/day05.rs
@@ -1,10 +1,11 @@
 use std::cmp::{max, min};
 use itertools::Itertools;
+use itertools::MinMaxResult::MinMax;
 use regex::Regex;
 
-pub fn solve_part_one(input: &String) -> i32 {
-    let re = Regex::new(r"(?m)(\d+),(\d+) -> (\d+),(\d+)").unwrap();
-    let lines = re.captures_iter(input)
+pub fn solve_part_one(input: &String) -> usize {
+    let hv_lines = Regex::new(r"(?m)(\d+),(\d+) -> (\d+),(\d+)").unwrap()
+        .captures_iter(input)
         .map(|capture| {
             let x1 = capture.get(1).unwrap().as_str().parse::<usize>().unwrap();
             let y1 = capture.get(2).unwrap().as_str().parse::<usize>().unwrap();
@@ -12,49 +13,31 @@ pub fn solve_part_one(input: &String) -> i32 {
             let y2 = capture.get(4).unwrap().as_str().parse::<usize>().unwrap();
             (min(x1, x2), min(y1, y2), max(x1, x2), max(y1, y2))
         })
-        .filter(|(x1, y1, x2, y2)| *x1 == *x2 || *y1 == *y2)
-        //.for_each(|x| ());
+        .filter(|(x1, y1, x2, y2)| x1 == x2 || y1 == y2)
         .collect_vec();
 
-    let x_min = lines.iter()
+    let (x_min, x_max) = hv_lines.iter()
         .flat_map(|(x1, _, x2, _)| [x1, x2])
-        .min()
-        .unwrap();
+        .minmax().into_option().unwrap();
 
-    let x_max = lines.iter()
-        .flat_map(|(x1, _, x2, _)| [x1, x2])
-        .max()
-        .unwrap();
-
-    let y_min = lines.iter()
+    let (y_min, y_max) = hv_lines.iter()
         .flat_map(|(_, y1, _, y2)| [y1, y2])
-        .min()
-        .unwrap();
+        .minmax().into_option().unwrap();
 
-    let y_max = lines.iter()
-        .flat_map(|(_, y1, _, y2)| [y1, y2])
-        .max()
-        .unwrap();
-
-    let mut grid = vec![[0; 1000]; 1000];
-
-    for (x1, y1, x2, y2) in lines {
-        for y in y1..=y2 {
-            for x in x1..=x2 {
-                grid[y][x] += 1;
+    let (height, width) = (x_max - x_min + 1, y_max - y_min + 1);
+    let grid = vec![vec![0; height]; width];
+    hv_lines.iter()
+        .fold(grid, |mut grid, (x1, y1, x2, y2)| {
+            for y in *y1..=*y2 {
+                for x in *x1..=*x2 {
+                    grid[y - y_min][x - x_min] += 1;
+                }
             }
-        }
-    }
-
-    let mut count = 0;
-    for row in grid {
-        for cell in row {
-            if cell >= 2 {
-                count += 1;
-            }
-        }
-    }
-    count
+            grid
+        }).iter()
+        .flat_map(|row| row)
+        .filter(|&freq| *freq >= 2)
+        .count()
 }
 
 pub fn solve_part_two(input: &String) -> i32 {
@@ -97,9 +80,7 @@ pub fn solve_part_two(input: &String) -> i32 {
             (min(y1, y2)..=max(y1, y2)).rev().collect_vec()
         };
 
-        dbg!((x1, y1, x2, y2));
         let coords = xs.iter().zip(ys.iter())
-            .map(|x| dbg!(x))
             .for_each(|(x, y)| {
                 grid[*y][*x] += 1;
             });
@@ -135,7 +116,7 @@ mod tests {
         0,0 -> 8,8
         5,5 -> 8,2
     "}.to_string(), 5)]
-    fn test_part_one(#[case] input: String, #[case] expected: i32) {
+    fn test_part_one(#[case] input: String, #[case] expected: usize) {
         assert_eq!(expected, solve_part_one(&input))
     }
 
