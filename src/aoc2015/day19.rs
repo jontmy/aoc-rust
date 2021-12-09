53e495b7d82816cbfbeb2a05130da077016c57ca
jontmy00 <jontmy001@gmail.com>
2021-12-09 12:50:35 +0800

    Solve 2015.19.2

--- src/aoc2015/day19.rs
+++ src/aoc2015/day19.rs
@@ -1,21 +1,59 @@
+use std::collections::{HashSet, VecDeque};
 use std::str::FromStr;
+
 use itertools::Itertools;
+use priority_queue::{DoublePriorityQueue, PriorityQueue};
 use regex::{Regex, Replacer};
 
 pub fn solve_part_one(input: &String) -> usize {
     let (replacements, molecule) = parse_input(input);
-
-    replacements.into_iter()
-        .flat_map(|replacement| molecule.all_replacements(&replacement))
-        .unique()
-        .count()
+    molecule.all_replacements(&replacements).len()
 }
 
 pub fn solve_part_two(input: &String) -> usize {
-    0
+    let (_, medicine) = parse_input(input);
+    let (caps, rn, ar, y) = ["[A-Z]", "Rn", "Ar", "Y"].into_iter()
+        .map(|condition| {
+            Regex::new(condition).unwrap()
+                .captures_iter(medicine.formula.as_str())
+                .count()
+        })
+        .collect_tuple().unwrap();
+
+    caps - rn - ar - 2 * y - 1
+
+    /*
+    // Brute force solution that will never return in a reasonable duration.
+
+    let primordial = Molecule::from_str("e").unwrap();
+    let (replacements, medicine) = parse_input(input);
+    let mut queue = DoublePriorityQueue::new();
+    queue.push((medicine.clone(), 0), medicine.clone().len());
+
+    // Invert the replacements so we get from the medicine molecule to the primordial electron "e".
+    let replacements = replacements.into_iter()
+        .map(Replacement::invert)
+        .collect_vec();
+
+    let mut calculated = HashSet::new();
+    loop {
+        let ((molecule, steps), _) = queue.pop_min().unwrap();
+        if molecule == primordial {
+            return steps;
+        }
+        dbg!((&molecule.len(), &molecule));
+        for molecule in molecule.all_replacements(&replacements).into_iter() {
+            if calculated.contains(&molecule) {
+                continue;
+            }
+            calculated.insert(molecule.clone());
+            queue.push((molecule.clone(), steps + 1), molecule.clone().len());
+        }
+    }
+    */
 }
 
-fn parse_input(input: &String) ->(Vec<Replacement>, Molecule) {
+fn parse_input(input: &String) -> (Vec<Replacement>, Molecule) {
     let (replacements, molecule): (&str, &str) = input.split("\n\n").collect_tuple().unwrap();
 
     let replacements = replacements.lines()
@@ -41,8 +79,18 @@ impl FromStr for Replacement {
             .collect_tuple().unwrap();
 
         Ok(Replacement {
-            input, output
+            input,
+            output,
         })
+    }
+}
+
+impl Replacement {
+    fn invert(self) -> Self {
+        Replacement {
+            input: self.output,
+            output: self.input,
+        }
     }
 }
 
@@ -55,17 +103,22 @@ impl FromStr for Molecule {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(Molecule{
+        Ok(Molecule {
             formula: s.trim().to_string(),
         })
     }
 }
 
 impl Molecule {
-    fn all_replacements(&self, replacement: &Replacement) -> Vec<Self> {
-        Regex::new(replacement.input.as_str()).unwrap()
-            .find_iter(self.formula.as_str())
-            .map(|m| self.replace(replacement, m.start(), m.end()))
+    fn all_replacements(&self, replacements: &[Replacement]) -> Vec<Self> {
+        replacements.into_iter()
+            .flat_map(|replacement| {
+                Regex::new(replacement.input.as_str()).unwrap()
+                    .find_iter(self.formula.as_str())
+                    .map(|m| self.replace(replacement, m.start(), m.end()))
+                    .collect_vec()
+            })
+            .unique()
             .collect_vec()
     }
 
@@ -76,6 +129,10 @@ impl Molecule {
         Molecule {
             formula
         }
+    }
+
+    fn len(&self) -> usize {
+        self.formula.len()
     }
 }
 
