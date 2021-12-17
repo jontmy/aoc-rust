4f709a165b4986ed39a8ccc220c7630ba700ed18
jontmy00 <jontmy001@gmail.com>
2021-12-17 15:18:15 +0800

    Merge branch `2021.17` into `master`
    commit 49099b21e57369cddc791b90dd4018daf8f3c0cf
    Author: jontmy00 <jontmy001@gmail.com>
    Date:   Fri Dec 17 15:17:33 2021 +0800
    
        Refactor 2021.17
    
    commit 40a3362440039e05a0051736f99bb9edbd837ecf
    Author: jontmy00 <jontmy001@gmail.com>
    Date:   Fri Dec 17 13:39:58 2021 +0800
    
        Solve 2021.17
    
    commit 1ad69af3586a72a33a217b2de6a9c45a309ac97e
    Author: jontmy00 <jontmy001@gmail.com>
    Date:   Fri Dec 17 13:39:49 2021 +0800
    
        Add 2021.17 input

--- src/aoc2021/day17.rs
+++ src/aoc2021/day17.rs
@@ -1,82 +1,124 @@
-use std::collections::HashSet;
-use itertools::Itertools;
+use std::str::FromStr;
+
+use itertools::{iterate, Itertools};
 use serde_scan::scan;
+
 use crate::utils::coordinates::dim_2::Coordinates;
 
-// REFACTOR IN PROGRESS
-
 pub fn solve_part_one(input: &String) -> i32 {
-    let (x_min, x_max, y_min, y_max): (i32, i32, i32, i32) = scan!("target area: x={}..{}, y={}..{}" <- input).unwrap();
-    let mut highest = 0;
-    for dx in 0..x_max {
-        for dy in 0..300 {
-            let probe = Probe { c: Coordinates::at(0, 0), dx, dy };
-            match probe.simulate(x_min, x_max, y_min, y_max) {
-                Some(height) => {
-                    highest = highest.max(height)
-                },
-                None => {
-                    highest = highest
-                },
-            }
-        }
-    }
-    highest
+    let target_area = input.parse::<TargetArea>().unwrap();
+    (0..=target_area.x_max)
+        .into_iter()
+        .cartesian_product(-1000..=1000)
+        .filter_map(|(dx, dy)| Probe::new(dx, dy).max_height(&target_area))
+        .max()
+        .unwrap()
 }
 
-#[derive(Clone, Copy)]
+pub fn solve_part_two(input: &String) -> usize {
+    let target_area = input.parse::<TargetArea>().unwrap();
+    (0..=target_area.x_max)
+        .into_iter()
+        .cartesian_product(-1000..=1000)
+        .filter_map(|(dx, dy)| Probe::new(dx, dy).max_height(&target_area))
+        .count()
+}
+
+struct TargetArea {
+    x_min: i32,
+    x_max: i32,
+    y_min: i32,
+    y_max: i32,
+}
+
+impl FromStr for TargetArea {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s = s.trim();
+        let (x_min, x_max, y_min, y_max): (i32, i32, i32, i32) =
+            scan!("target area: x={}..{}, y={}..{}" <- s).unwrap();
+
+        Ok(TargetArea {
+            x_min,
+            x_max,
+            y_min,
+            y_max,
+        })
+    }
+}
+
+impl TargetArea {
+    fn hit_by(&self, c: Coordinates) -> bool {
+        c.x() >= self.x_min && c.x() <= self.x_max && c.y() >= self.y_min && c.y() <= self.y_max
+    }
+}
+
+#[derive(Debug)]
 struct Probe {
-    c: Coordinates,
+    coords: Coordinates,
     dx: i32,
     dy: i32,
 }
 
 impl Probe {
-    fn step(self) -> Self {
-        let c = self.c.translate(self.dx, self.dy);
-        let dx = if self.dx == 0 { 0 } else if self.dx > 0 { self.dx - 1 } else { self.dx + 1 };
-        let dy = self.dy - 1;
-
-        Probe { c, dx, dy }
-    }
-
-    fn simulate(&self, x_min: i32, x_max: i32, y_min: i32, y_max: i32) -> Option<i32> {
-        let mut probe = self.clone();
-        let mut highest = self.c.y();
-        loop {
-            probe = probe.step();
-            highest = highest.max(probe.c.y());
-            if probe.c.x() > x_max || probe.c.y() < y_min {
-                return None;
-            } else if probe.c.x() < x_min && probe.dx == 0 {
-                return None;
-            } else if probe.c.x() >= x_min && probe.c.x() <= x_max && probe.c.y() >= y_min && probe.c.y() <= y_max {
-                return Some(highest);
-            }
+    /// Returns a new `Probe` positioned at the origin, given its initial `x-` and `y-`velocities.
+    fn new(dx: i32, dy: i32) -> Self {
+        Probe {
+            coords: Coordinates::at(0, 0),
+            dx,
+            dy,
         }
     }
-}
 
-
-
-pub fn solve_part_two(input: &String) -> i32 {
-    let (x_min, x_max, y_min, y_max): (i32, i32, i32, i32) = scan!("target area: x={}..{}, y={}..{}" <- input).unwrap();
-    let mut reached = 0;
-    for dx in -1000..1000 {
-        for dy in -1000..1000 {
-            // dbg!((dx, dy));
-            let probe = Probe { c: Coordinates::at(0, 0), dx, dy };
-            match probe.simulate(x_min, x_max, y_min, y_max) {
-                Some(height) => {
-                    reached += 1;
-                },
-                None => {
-                    reached += 0;
-                },
-            }
-        }
+    /// Returns the x-component of this `Probe`'s coordinates.
+    fn x(&self) -> i32 {
+        self.coords.x()
     }
-    reached
+
+    /// Returns the y-component of this `Probe`'s coordinates.
+    fn y(&self) -> i32 {
+        self.coords.y()
+    }
+
+    /// Returns an infinite iterator of `Probe`s at each step of this `Probe`'s trajectory.
+    fn trajectory(self) -> impl Iterator<Item = Probe> {
+        iterate(self, |probe| {
+            let coordinates = probe.coords.translate(probe.dx, probe.dy);
+            let x_velocity = probe.dx - probe.dx.signum();
+            let y_velocity = probe.dy - 1;
+
+            Probe {
+                coords: coordinates,
+                dx: x_velocity,
+                dy: y_velocity,
+            }
+        })
+    }
+
+    /// Returns the highest `y`-position reached along this `Probe`'s trajectory if and only if
+    /// this `Probe` enters a given target area along said trajectory.
+    fn max_height(self, target: &TargetArea) -> Option<i32> {
+        let trajectory = self
+            .trajectory()
+            .take_while(|probe| {
+                probe.x() <= target.x_max
+                    && probe.y() >= target.y_min
+                    && !(probe.dx == 0 && probe.x() < target.x_min)
+            })
+            .collect_vec();
+
+        let hit_target = trajectory
+            .last()
+            .map(|probe| target.hit_by(probe.coords))
+            .unwrap_or(false);
+
+        return if hit_target {
+            trajectory.into_iter().map(|probe| probe.y()).max()
+        } else {
+            None
+        };
+    }
 }
 
 #[cfg(test)]
@@ -87,8 +129,8 @@ mod tests {
 
     #[rstest]
     #[case(indoc::indoc ! {"
-
-    "}.to_string(), 0)]
+        target area: x=20..30, y=-10..-5
+    "}.to_string(), 45)]
     fn test_part_one(#[case] input: String, #[case] expected: i32) {
         assert_eq!(expected, solve_part_one(&input))
     }
@@ -97,7 +139,7 @@ mod tests {
     #[case(indoc::indoc ! {"
 
     "}.to_string(), 0)]
-    fn test_part_two(#[case] input: String, #[case] expected: i32) {
+    fn test_part_two(#[case] input: String, #[case] expected: usize) {
         assert_eq!(expected, solve_part_two(&input))
     }
 }
