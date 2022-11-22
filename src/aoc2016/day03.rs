a2ba348ba8dfdfc17e63efafbfbc5a0002765004
Jonathan Tay <jonathantaymy@gmail.com>
2022-11-22 15:55:06 +0800

    feat: solve 2016.3.2

--- src/aoc2016/day03.rs
+++ src/aoc2016/day03.rs
@@ -19,6 +19,17 @@ impl Triangle {
     fn is_valid(&self) -> bool {
         self.0 + self.1 > self.2 && self.0 + self.2 > self.1 && self.1 + self.2 > self.0
     }
+
+    fn from_group(group: itertools::Chunk<std::str::Lines>) -> Vec<Self> {
+        let lengths = group.flat_map(|line| line.split_ascii_whitespace())
+            .map(|length| length.parse().unwrap())
+            .collect_vec();
+        vec![
+            Triangle(lengths[0], lengths[3], lengths[6]),
+            Triangle(lengths[1], lengths[4], lengths[7]),
+            Triangle(lengths[2], lengths[5], lengths[8])
+        ]
+    }
 }
 
 pub fn solve_part_one(input: String) -> usize {
@@ -28,6 +39,11 @@ pub fn solve_part_one(input: String) -> usize {
         .count()
 }
 
-pub fn solve_part_two(input: String) -> String {
-    "".to_string()
+pub fn solve_part_two(input: String) -> usize {
+    input.lines()
+        .chunks(3)
+        .into_iter()
+        .flat_map(|group| Triangle::from_group(group))
+        .filter(|triangle| triangle.is_valid())
+        .count()
 }
