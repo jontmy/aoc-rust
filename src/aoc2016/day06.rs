327515200fb9a1d13b95db343c07ddc5c118375c
Jonathan Tay <jonathantaymy@gmail.com>
2022-11-23 16:43:03 +0800

    feat: solve 2016.6.2

--- src/aoc2016/day06.rs
+++ src/aoc2016/day06.rs
@@ -24,7 +24,24 @@ pub fn solve_part_one(input: String) -> String {
 }
 
 pub fn solve_part_two(input: String) -> String {
-    "".to_string()
+    input
+        .lines()
+        .flat_map(|line| line.chars().enumerate()) // take every character in the input and convert it into (column index, character)
+        .into_group_map_by(|(col, _)| *col) // now group into a map every column index to a vector of (column index, character)
+        .into_iter()
+        .sorted_by_key(|(col, _)| *col) // we lose the column index order in the iterator during the grouping, so restore it
+        .map(|(_, letters)| {
+            let frequencies = letters // map each character in this column to its frequency
+                .into_iter()
+                .map(|(_, letter)| letter)
+                .counts();
+            let (letter, _) = frequencies // find the least common letter in this column and return it
+                .into_iter()
+                .min_by(|(a, a_freq), (b, b_freq)| a_freq.cmp(b_freq))
+                .unwrap();
+            letter
+        })
+        .collect() // concatenate the most common letter from each column
 }
 
 #[cfg(test)]
@@ -64,7 +81,31 @@ mod tests {
     }
 
     #[rstest]
-    #[case("abc".to_string(), "05ace8e3".to_string())]
+    #[case(
+        (
+            indoc::indoc! {
+                "
+        eedadn
+        drvtee
+        eandsr
+        raavrd
+        atevrs
+        tsrnev
+        sdttsa
+        rasrtv
+        nssdts
+        ntnada
+        svetve
+        tesnvt
+        vntsnd
+        vrdear
+        dvrsen
+        enarar
+    "
+            }
+        ).to_string(),
+        "advent".to_string()
+    )]
     fn test_solve_part_two(#[case] input: String, #[case] expected: String) {
         assert_eq!(solve_part_two(input), expected);
     }
