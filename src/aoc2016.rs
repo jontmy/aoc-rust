87f5fb566e025e34e724549714730a088695b7b3
Jonathan Tay <jonathantaymy@gmail.com>
2022-11-23 16:38:55 +0800

    feat: solve 2016.6.1

--- src/aoc2016.rs
+++ src/aoc2016.rs
@@ -3,6 +3,7 @@ use crate::utils::io;
 mod day03;
 mod day04;
 mod day05;
+mod day06;
 
 pub fn run() {
     io::write(3, 2016,
@@ -13,8 +14,13 @@ pub fn run() {
         day04::solve_part_one(io::read(4, 2016)),
         day04::solve_part_two(io::read(4, 2016))
     );
-    io::write(5, 2016,
-        day05::solve_part_one(io::read(5, 2016)),
-        day05::solve_part_two(io::read(5, 2016))
+    // day 5 skipped - solution takes > 60s to derive
+    // io::write(5, 2016,
+    //     day05::solve_part_one(io::read(5, 2016)),
+    //     day05::solve_part_two(io::read(5, 2016))
+    // );
+    io::write(6, 2016,
+        day06::solve_part_one(io::read(6, 2016)),
+        day06::solve_part_two(io::read(6, 2016))
     );
 }
---
+++ src/aoc2016/day06.rs
@@ -0,0 +1,71 @@
+use std::{ collections::HashMap, fmt::format, str::FromStr };
+
+use itertools::Itertools;
+
+pub fn solve_part_one(input: String) -> String {
+    input
+        .lines()
+        .flat_map(|line| line.chars().enumerate()) // take every character in the input and convert it into (column index, character)
+        .into_group_map_by(|(col, _)| *col) // now group into a map every column index to a vector of (column index, character)
+        .into_iter()
+        .sorted_by_key(|(col, _)| *col) // we lose the column index order in the iterator during the grouping, so restore it
+        .map(|(_, letters)| {
+            let frequencies = letters // map each character in this column to its frequency
+                .into_iter()
+                .map(|(_, letter)| letter)
+                .counts();
+            let (letter, _) = frequencies // find the most common letter in this column and return it
+                .into_iter()
+                .max_by(|(a, a_freq), (b, b_freq)| a_freq.cmp(b_freq))
+                .unwrap();
+            letter
+        })
+        .collect() // concatenate the most common letter from each column
+}
+
+pub fn solve_part_two(input: String) -> String {
+    "".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+
+    use super::{ solve_part_one, solve_part_two };
+
+    #[rstest]
+    #[case(
+        (
+            indoc::indoc! {
+                "
+        eedadn
+        drvtee
+        eandsr
+        raavrd
+        atevrs
+        tsrnev
+        sdttsa
+        rasrtv
+        nssdts
+        ntnada
+        svetve
+        tesnvt
+        vntsnd
+        vrdear
+        dvrsen
+        enarar
+    "
+            }
+        ).to_string(),
+        "easter".to_string()
+    )]
+    fn test_solve_part_one(#[case] input: String, #[case] expected: String) {
+        assert_eq!(solve_part_one(input), expected)
+    }
+
+    #[rstest]
+    #[case("abc".to_string(), "05ace8e3".to_string())]
+    fn test_solve_part_two(#[case] input: String, #[case] expected: String) {
+        assert_eq!(solve_part_two(input), expected);
+    }
+}
