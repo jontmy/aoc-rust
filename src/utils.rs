5832a7635dda6a869122e3fc8d2a12e8b274b686
Jonathan Tay <jonathantaymy@gmail.com>
2022-11-29 15:40:17 +0800

    feat: implement `Solver` trait

--- src/utils.rs
+++ src/utils.rs
@@ -2,3 +2,4 @@
 
 pub mod io;
 pub mod coordinates;
+pub mod advent;
---
+++ src/utils/advent.rs
@@ -0,0 +1,69 @@
+use std::{ fmt::Display, error::Error, path::Path, fs, fs::{ File, OpenOptions }, io::Write };
+
+use ansi_term::Style;
+
+pub trait Solver<const YEAR: u32, const DAY: u32> {
+    type Error;
+    fn solve_part_one(&self, input: &str) -> String;
+    fn solve_part_two(&self, input: &str) -> String;
+    
+    fn solve(&self) {
+        // Use const generics to get the details of this puzzle from structs which implement this trait.
+        let day = DAY;
+        let year = YEAR;
+        println!(
+            "\n{}\n",
+            Style::new().bold().paint(format!("Advent of Code {}, Day {}", year, day))
+        );
+
+        // Create the input file (and its parent directories).
+        let input_path = format!("input/{}/{:02}.txt", year, day);
+        let input_path = Path::new(&input_path);
+        let input_dir = input_path
+            .parent()
+            .expect("Failed to get input parent directory for {year}");
+
+        if !input_dir.exists() {
+            println!("Input folder for {year} does not exist, creating it...");
+        }
+        fs::create_dir_all(input_dir).expect("Failed to create input folder for {year}");
+        if !input_path.exists() {
+            println!("Input file for day {day} of {year} does not exist, creating it...");
+        }
+        File::create(&input_path).expect("Failed to create input file for day {day} of {year}");
+
+        // Read the input file.
+        let input = fs::read_to_string(input_path).expect("Failed to read input file");
+
+        // Create the output file (and its parent directories).
+        let output_path = format!("output/{}/{:02}.txt", year, day);
+        let output_path = Path::new(&output_path);
+        let output_dir = output_path
+            .parent()
+            .expect("Failed to get output parent directory for year {year}");
+
+        fs::create_dir_all(output_dir).expect("Failed to create output folder for {year}");
+        File::create(&output_path).expect("Failed to create output file for day {day} of {year}");
+
+        let mut file = OpenOptions::new()
+            .append(true)
+            .open(output_path)
+            .expect("Failed to open output file for day {day} of {year}");
+
+        // Solve part 1, writing to stdout and the output file.
+        print!("Part 1: ");
+        let part_one_solution = self.solve_part_one(&input);
+        println!("{}", Style::new().italic().paint(format!("{}", part_one_solution)));
+        writeln!(file, "{part_one_solution}").expect(
+            "Failed to write part 1 solution for day {day} of {year}"
+        );
+
+        // Solve part 2, writing to stdout and the output file.
+        print!("Part 2: ");
+        let part_two_solution = self.solve_part_two(&input);
+        println!("{}", Style::new().italic().paint(format!("{}", part_two_solution)));
+        writeln!(file, "{part_two_solution}").expect(
+            "Failed to write part 2 solution for day {day} of {year}"
+        );
+    }
+}
