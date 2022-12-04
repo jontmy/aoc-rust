9582b51555c2bd02cd3d51a2c3d769adcc736a7e
Jonathan Tay <jonathantaymy@gmail.com>
2022-12-04 13:28:29 +0800

    refactor: cleanup 2022.04

--- src/aoc2022/day04.rs
+++ src/aoc2022/day04.rs
@@ -1,7 +1,6 @@
-use itertools::Itertools;
-use once_cell_regex::regex;
-
 use crate::utils::advent;
+
+use scan_fmt::scan_fmt;
 
 pub struct Solver;
 
@@ -10,38 +9,18 @@ impl advent::Solver<2022, 4> for Solver {
     type Part2 = usize;
 
     fn solve_part_one(&self, input: &str) -> Self::Part1 {
-        let regex = regex!(r"(\d+)-(\d+),(\d+)-(\d+)");
-        let mut count = 0;
-        for line in input.lines() {
-            print!("{line}   ");
-            let captures = regex.captures(line).unwrap();
-            let a = captures.get(1).unwrap().as_str().parse::<i32>().unwrap();
-            let b = captures.get(2).unwrap().as_str().parse::<i32>().unwrap();
-            let c = captures.get(3).unwrap().as_str().parse::<i32>().unwrap();
-            let d = captures.get(4).unwrap().as_str().parse::<i32>().unwrap();
-            if (a <= c && b >= d) || (c <= a && d >= b) {
-                count += 1
-            }
-            println!("{:?}", (a <= c && b >= d) || (c <= a && d >= b))
-        }
-        count
+        input
+            .lines()
+            .filter_map(|l| scan_fmt!(l, "{d}-{d},{d}-{d}", i32, i32, i32, i32).ok())
+            .filter(|(a, b, c, d)| ((a <= c && b >= d) || (c <= a && d >= b)))
+            .count()
     }
 
     fn solve_part_two(&self, input: &str) -> Self::Part2 {
-        let regex = regex!(r"(\d+)-(\d+),(\d+)-(\d+)");
-        let mut count = 0;
-        for line in input.lines() {
-            print!("{line}   ");
-            let captures = regex.captures(line).unwrap();
-            let a = captures.get(1).unwrap().as_str().parse::<i32>().unwrap();
-            let b = captures.get(2).unwrap().as_str().parse::<i32>().unwrap();
-            let c = captures.get(3).unwrap().as_str().parse::<i32>().unwrap();
-            let d = captures.get(4).unwrap().as_str().parse::<i32>().unwrap();
-            if (a < c && b < c) || (a > d && b > d) || (d < a && c < a) || (c > b && c > a) {
-                count += 1
-            }
-            println!("{:?}", (a < c && b < c) || (a > d && b > d))
-        }
-        1000 - count
+        input
+            .lines()
+            .filter_map(|l| scan_fmt!(l, "{d}-{d},{d}-{d}", i32, i32, i32, i32).ok())
+            .filter(|(a, b, c, d)| ((a <= c && c <= b) || (c <= a && a <= d)))
+            .count()
     }
 }
