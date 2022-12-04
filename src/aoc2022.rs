efd09713f157c2c55327b2e970257782ab83e30a
Jonathan Tay <jonathantaymy@gmail.com>
2022-12-04 13:15:16 +0800

    feat: solve 2022.04

--- src/aoc2022.rs
+++ src/aoc2022.rs
@@ -3,9 +3,11 @@ use crate::utils::advent::Solver;
 mod day01;
 mod day02;
 mod day03;
+mod day04;
 
 pub fn run() {
     day01::Solver.solve();
     day02::Solver.solve();
     day03::Solver.solve();
+    day04::Solver.solve();
 }
