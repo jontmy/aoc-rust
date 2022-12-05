d1799d4406420f683921cdbcfb878970db5cd27c
Jonathan Tay <jonathantaymy@gmail.com>
2022-12-05 13:36:39 +0800

    fix: generalize 2022.5 for all inputs

--- src/aoc2022/day05.rs
+++ src/aoc2022/day05.rs
@@ -1,4 +1,4 @@
-use std::{collections::VecDeque, vec};
+use std::{ collections::VecDeque, vec };
 
 use crate::utils::advent;
 
@@ -7,24 +7,41 @@ use scan_fmt::scan_fmt;
 
 pub struct Solver;
 
+impl Solver {
+    fn parse(input: &str) -> [VecDeque<char>; 9] {
+        let crates = input
+            .lines()
+            .take_while(|l| l.starts_with("["))
+            .flat_map(|l|
+                l
+                    .chars()
+                    .skip(1)
+                    .step_by(4)
+                    .enumerate()
+                    .filter(|(_, c)| c.is_alphabetic())
+            )
+            .into_grouping_map()
+            .collect::<VecDeque<char>>();
+
+        let crates: [VecDeque<char>; 9] = crates
+            .into_iter()
+            .sorted_by_key(|(i, _)| *i)
+            .map(|(_, stack)| stack)
+            .collect_vec()
+            .try_into()
+            .unwrap();
+
+        crates
+    }
+}
+
 impl advent::Solver<2022, 5> for Solver {
     type Part1 = usize;
     type Part2 = usize;
 
     fn solve_part_one(&self, input: &str) -> Self::Part1 {
-        let mut stacks = vec![
-            VecDeque::from_iter("MSJLVFNR".chars()),
-            VecDeque::from_iter("HWJFZDNP".chars()),
-            VecDeque::from_iter("GDCRW".chars()),
-            VecDeque::from_iter("SBN".chars()),
-            VecDeque::from_iter("NFBCPWZM".chars()),
-            VecDeque::from_iter("WMRP".chars()),
-            VecDeque::from_iter("WSLGNTR".chars()),
-            VecDeque::from_iter("VBNFHTQ".chars()),
-            VecDeque::from_iter("FNZHML".chars()),
-        ];
+        let mut stacks = Solver::parse(input);
         let (_, ins) = input.split("\n\n").collect_tuple::<(_, _)>().unwrap();
-        println!("{ins}");
         let instructions = ins
             .lines()
             .filter_map(|l| scan_fmt!(l, "move {d} from {d} to {d}", usize, usize, usize).ok())
@@ -42,19 +59,8 @@ impl advent::Solver<2022, 5> for Solver {
     }
 
     fn solve_part_two(&self, input: &str) -> Self::Part2 {
-        let mut stacks = vec![
-            VecDeque::from_iter("MSJLVFNR".chars()),
-            VecDeque::from_iter("HWJFZDNP".chars()),
-            VecDeque::from_iter("GDCRW".chars()),
-            VecDeque::from_iter("SBN".chars()),
-            VecDeque::from_iter("NFBCPWZM".chars()),
-            VecDeque::from_iter("WMRP".chars()),
-            VecDeque::from_iter("WSLGNTR".chars()),
-            VecDeque::from_iter("VBNFHTQ".chars()),
-            VecDeque::from_iter("FNZHML".chars()),
-        ];
+        let mut stacks = Solver::parse(input);
         let (_, ins) = input.split("\n\n").collect_tuple::<(_, _)>().unwrap();
-        println!("{ins}");
         let instructions = ins
             .lines()
             .filter_map(|l| scan_fmt!(l, "move {d} from {d} to {d}", usize, usize, usize).ok())
