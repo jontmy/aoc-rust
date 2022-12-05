00acf215c4cede2c5c4b0fdc8d553fdeb203bfd7
Jonathan Tay <jonathantaymy@gmail.com>
2022-12-05 13:55:48 +0800

    refactor: cleanup 2022.05

--- src/aoc2022/day05.rs
+++ src/aoc2022/day05.rs
@@ -1,14 +1,20 @@
-use std::{ collections::VecDeque, vec };
+use std::collections::VecDeque;
 
 use crate::utils::advent;
 
 use itertools::Itertools;
 use scan_fmt::scan_fmt;
 
+struct Instruction {
+    qty: usize,
+    from: usize,
+    to: usize,
+}
+
 pub struct Solver;
 
 impl Solver {
-    fn parse(input: &str) -> [VecDeque<char>; 9] {
+    fn parse(input: &str) -> (Vec<VecDeque<char>>, impl Iterator<Item = Instruction> + '_) {
         let crates = input
             .lines()
             .take_while(|l| l.starts_with("["))
@@ -23,62 +29,56 @@ impl Solver {
             .into_grouping_map()
             .collect::<VecDeque<char>>();
 
-        let crates: [VecDeque<char>; 9] = crates
+        let crates = crates
             .into_iter()
             .sorted_by_key(|(i, _)| *i)
             .map(|(_, stack)| stack)
-            .collect_vec()
-            .try_into()
-            .unwrap();
+            .collect();
 
-        crates
+        let instructions = input
+            .lines()
+            .skip_while(|l| !l.starts_with("move"))
+            .filter_map(|l| scan_fmt!(l, "move {d} from {d} to {d}", usize, usize, usize).ok())
+            .map(|(qty, from, to)| Instruction { qty, from: from - 1, to: to - 1 });
+
+        (crates, instructions)
     }
 }
 
 impl advent::Solver<2022, 5> for Solver {
-    type Part1 = usize;
-    type Part2 = usize;
+    type Part1 = String;
+    type Part2 = String;
 
     fn solve_part_one(&self, input: &str) -> Self::Part1 {
-        let mut stacks = Solver::parse(input);
-        let (_, ins) = input.split("\n\n").collect_tuple::<(_, _)>().unwrap();
-        let instructions = ins
-            .lines()
-            .filter_map(|l| scan_fmt!(l, "move {d} from {d} to {d}", usize, usize, usize).ok())
-            .collect_vec();
-        for (amt, from, to) in instructions {
-            for _ in 0..amt {
-                let item = stacks[from - 1].pop_front().unwrap();
-                stacks[to - 1].push_front(item);
+        let (mut stacks, instructions) = Solver::parse(input);
+        for ins in instructions {
+            for _ in 0..ins.qty {
+                let item = stacks[ins.from].pop_front().unwrap();
+                stacks[ins.to].push_front(item);
             }
         }
-        for i in 0..9 {
-            print!("{:?}", stacks[i].pop_front().unwrap());
-        }
-        0
+        stacks
+            .into_iter()
+            .filter_map(|mut stack| stack.pop_front())
+            .collect()
     }
 
     fn solve_part_two(&self, input: &str) -> Self::Part2 {
-        let mut stacks = Solver::parse(input);
-        let (_, ins) = input.split("\n\n").collect_tuple::<(_, _)>().unwrap();
-        let instructions = ins
-            .lines()
-            .filter_map(|l| scan_fmt!(l, "move {d} from {d} to {d}", usize, usize, usize).ok())
-            .collect_vec();
+        let (mut stacks, instructions) = Solver::parse(input);
         let mut holding = VecDeque::new();
-        for (amt, from, to) in instructions {
-            for _ in 0..amt {
-                let item = stacks[from - 1].pop_front().unwrap();
+        for ins in instructions {
+            for _ in 0..ins.qty {
+                let item = stacks[ins.from].pop_front().unwrap();
                 holding.push_back(item);
             }
-            for _ in 0..amt {
+            for _ in 0..ins.qty {
                 let item = holding.pop_back().unwrap();
-                stacks[to - 1].push_front(item);
+                stacks[ins.to].push_front(item);
             }
         }
-        for i in 0..9 {
-            print!("{:?}", stacks[i].pop_front().unwrap());
-        }
-        0
+        stacks
+            .into_iter()
+            .filter_map(|mut stack| stack.pop_front())
+            .collect()
     }
 }
