c8258a42a090acec706ab32b5881d48f5cbd6e17
Jonathan Tay <jonathantaymy@gmail.com>
2022-12-06 13:19:12 +0800

    refactor: reduce code duplication

--- src/aoc2022/day06.rs
+++ src/aoc2022/day06.rs
@@ -1,33 +1,30 @@
-use std::collections::HashSet;
-
 use crate::utils::advent;
 
 use itertools::Itertools;
-use scan_fmt::scan_fmt;
 
 pub struct Solver;
+
+impl Solver {
+    fn find_marker(input: &str, n: usize) -> usize {
+        let input = input.trim();
+        for i in 0..input.len() - n {
+            if input[i..i + n].chars().unique().count() == n {
+                return i + n;
+            }
+        }
+        panic!()
+    }
+}
 
 impl advent::Solver<2022, 6> for Solver {
     type Part1 = usize;
     type Part2 = usize;
 
     fn solve_part_one(&self, input: &str) -> Self::Part1 {
-        let input = input.trim();
-        for i in 0..input.len() - 4 {
-            if input[i..i + 4].chars().unique().count() == 4 {
-                return i + 4;
-            }
-        }
-        unreachable!()
+        Solver::find_marker(input, 4)
     }
 
     fn solve_part_two(&self, input: &str) -> Self::Part2 {
-        let input = input.trim();
-        for i in 0..input.len() - 14 {
-            if input[i..i + 14].chars().unique().count() == 14 {
-                return i + 14;
-            }
-        }
-        unreachable!()
+        Solver::find_marker(input, 14)
     }
 }
