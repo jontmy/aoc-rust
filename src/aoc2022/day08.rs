a9aaacfa7abd710678a8a5c8878749fb18c3b0cd
Jonathan Tay <jonathantaymy@gmail.com>
2022-12-08 17:12:12 +0800

    refactor: cleanup 2022.08

--- src/aoc2022/day08.rs
+++ src/aoc2022/day08.rs
@@ -1,8 +1,23 @@
-use crate::utils::advent;
+use crate::utils::{ advent, grid::Grid };
 
 use itertools::Itertools;
 
 pub struct Solver;
+
+impl Solver {
+    fn parse(input: &str) -> Grid<u32> {
+        input
+            .trim()
+            .lines()
+            .map(|x|
+                x
+                    .chars()
+                    .filter_map(|d| d.to_digit(10))
+                    .collect_vec()
+            )
+            .collect::<Grid<_>>()
+    }
+}
 
 // perf: brute forced
 impl advent::Solver<2022, 8> for Solver {
@@ -10,104 +25,50 @@ impl advent::Solver<2022, 8> for Solver {
     type Part2 = usize;
 
     fn solve_part_one(&self, input: &str) -> Self::Part1 {
-        let s = input
-            .trim()
-            .lines()
-            .map(|x|
-                x
-                    .chars()
-                    .map(|d| d.to_digit(10).unwrap())
-                    .collect_vec()
-            )
-            .collect_vec();
+        let input = Solver::parse(input);
+        let grid = input.clone().shrink();
 
-        let h = s.len();
-        let w = s.first().unwrap().len();
-        let mut total = h + h + w - 2 + w - 2; // + perimeter
-        for y in 1..h - 1 {
-            for x in 1..w - 1 {
-                let c = s[y][x];
-                let mut d = (true, true, true, true);
-                for i in 0..y {
-                    // up
-                    if s[i][x] >= c {
-                        d.0 = false;
-                    }
-                }
-                for i in y + 1..h {
-                    // down
-                    if s[i][x] >= c {
-                        d.1 = false;
-                    }
-                }
-                for i in 0..x {
-                    // left
-                    if s[y][i] >= c {
-                        d.2 = false;
-                    }
-                }
-                for i in x + 1..w {
-                    // right
-                    if s[y][i] >= c {
-                        d.3 = false;
-                    }
-                }
-                total += if d.0 || d.1 || d.2 || d.3 { 1 } else { 0 };
-            }
-        }
-        total
+        let visible_count = grid
+            .clone()
+            .enumerated_map(|c, h| {
+                let (left, _, right) = grid.row_split_at(c.x(), c.y());
+                let (above, _, below) = grid.col_split_at(c.x(), c.y());
+                let dirs = vec![left, right, above, below];
+
+                dirs.into_iter()
+                    .map(|dir| dir.into_iter().all(|height| *height < h))
+                    .filter(|is_visible| *is_visible)
+                    .count()
+                    .min(1)
+            })
+            .into_iter()
+            .sum::<usize>();
+
+        visible_count + grid.perimeter() // all trees arond the edge of the grid are visible
     }
 
     fn solve_part_two(&self, input: &str) -> Self::Part2 {
-        let s = input
-            .trim()
-            .lines()
-            .map(|x|
-                x
-                    .chars()
-                    .map(|d| d.to_digit(10).unwrap())
-                    .collect_vec()
-            )
-            .collect_vec();
+        let input = Solver::parse(input);
+        let grid = input.clone().shrink();
 
-        let h = s.len();
-        let w = s.first().unwrap().len();
-        let mut max = 0;
-        for y in 1..h - 1 {
-            for x in 1..w - 1 {
-                let c = s[y][x];
-                let mut d = (0, 0, 0, 0);
-                for i in (0..y).rev() {
-                    // up
-                    d.0 += 1;
-                    if s[i][x] >= c {
-                        break;
-                    }
-                }
-                for i in y + 1..h {
-                    // down
-                    d.1 += 1;
-                    if s[i][x] >= c {
-                        break;
-                    }
-                }
-                for i in (0..x).rev() {
-                    // left
-                    d.2 += 1;
-                    if s[y][i] >= c {
-                        break;
-                    }
-                }
-                for i in x + 1..w {
-                    // right
-                    d.3 += 1;
-                    if s[y][i] >= c {
-                        break;
-                    }
-                }
-                max = max.max(d.0 * d.1 * d.2 * d.3);
-            }
-        }
-        max
+        grid.clone()
+            .enumerated_map(|c, h| {
+                let (left, _, right) = grid.row_split_at(c.x(), c.y());
+                let (above, _, below) = grid.col_split_at(c.x(), c.y());
+                let dirs = vec![left, right, above, below];
+
+                dirs.into_iter()
+                    .map(|dir| {
+                        let max_dist = dir.len() + 1; // if this tree is tallest in the direction
+                        dir.into_iter()
+                            .find_position(|height| **height >= h)
+                            .map(|(i, _)| i + 1)
+                            .unwrap_or(max_dist)
+                    })
+                    .product()
+            })
+            .into_iter()
+            .max()
+            .unwrap_or(0)
     }
 }
