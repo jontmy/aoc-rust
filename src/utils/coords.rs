d9d6b0c84524b8bcc36e66eb5bb3088348172aff
Jonathan Tay <jonathantaymy@gmail.com>
2022-12-10 11:55:50 +0800

    feat: add checked and wrapping coords utils

--- src/utils/coords.rs
+++ src/utils/coords.rs
@@ -1,6 +1,15 @@
-use std::{ hash::Hash, ops::{ Add, Sub, Mul }, fmt::{ Display, Formatter } };
-use super::directions::Direction;
-use num::{ Num, Integer, PrimInt, Signed };
+use std::{ hash::Hash, ops::{ Add, Sub, Mul, RangeBounds, Bound }, fmt::{ Display, Formatter } };
+use super::{ directions::Direction, misc };
+use num::{
+    Num,
+    Integer,
+    PrimInt,
+    Signed,
+    CheckedAdd,
+    CheckedSub,
+    traits::{ WrappingAdd, Euclid, WrappingSub },
+    iter::Range,
+};
 
 /// A point in 2D space which can either be signed or unsigned.
 /// The `x` and `y` fields are private; use the `x()` and `y()` methods to access them.
@@ -26,6 +35,184 @@ impl<T> Coordinates<T> where T: Num + Copy {
 
     pub fn y(&self) -> T {
         self.y
+    }
+}
+
+/// Utility methods for signed or unsigned coordinates, useful for checking that a coordinate won't go out of bounds of a grid.
+impl<T> Coordinates<T> where T: Integer + Copy + CheckedAdd + CheckedSub {
+    pub fn try_up(&self) -> Option<Self> {
+        self.y.checked_add(&num::one()).map(|y| Self { x: self.x, y })
+    }
+
+    pub fn try_up_by(&self, n: T) -> Option<Self> {
+        self.y.checked_add(&n).map(|y| Self { x: self.x, y })
+    }
+
+    /// Increments the `y` coordinate by 1, returning `None` if it goes out of bounds.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use advent_of_code::utils::coords::Coordinates;
+    /// assert_eq!(Coordinates::new(0, 0).try_bounded_up_by(1, ..=2), Some(Coordinates::new(0, 1)));
+    /// assert_eq!(Coordinates::new(0, 2).try_bounded_up_by(1, ..=2), None);
+    /// ```
+    pub fn try_bounded_up_by<R>(&self, n: T, y_range: R) -> Option<Self> where R: RangeBounds<T> {
+        self.y
+            .checked_add(&n)
+            .filter(|y| y_range.contains(y))
+            .map(|y| (self.x, y).into())
+    }
+
+    pub fn try_down(&self) -> Option<Self> {
+        self.y.checked_sub(&num::one()).map(|y| Self { x: self.x, y })
+    }
+
+    pub fn try_down_by(&self, n: T) -> Option<Self> {
+        self.y.checked_sub(&n).map(|y| Self { x: self.x, y })
+    }
+
+    /// Decrements the `y` coordinate by 1, returning `None` if it goes out of bounds.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use advent_of_code::utils::coords::Coordinates;
+    /// assert_eq!(Coordinates::new(0, 2).try_bounded_down_by(1, 0..), Some(Coordinates::new(0, 1)));
+    /// assert_eq!(Coordinates::new(0, 0).try_bounded_down_by(1, 0..), None);
+    /// ```
+    pub fn try_bounded_down_by<R>(&self, n: T, y_range: R) -> Option<Self> where R: RangeBounds<T> {
+        self.y
+            .checked_sub(&n)
+            .filter(|y| y_range.contains(y))
+            .map(|y| (self.x, y).into())
+    }
+
+    pub fn try_right(&self) -> Option<Self> {
+        self.x.checked_add(&num::one()).map(|x| Self { x, y: self.y })
+    }
+
+    pub fn try_right_by(&self, n: T) -> Option<Self> {
+        self.x.checked_add(&n).map(|x| Self { x, y: self.y })
+    }
+
+    /// Increments the `x` coordinate by 1, returning `None` if it goes out of bounds.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use advent_of_code::utils::coords::Coordinates;
+    /// assert_eq!(Coordinates::new(0, 0).try_bounded_right_by(1, ..=2), Some(Coordinates::new(1, 0)));
+    /// assert_eq!(Coordinates::new(2, 0).try_bounded_right_by(1, ..=2), None);
+    /// ```
+    pub fn try_bounded_right_by<R>(&self, n: T, x_range: R) -> Option<Self> where R: RangeBounds<T> {
+        self.x
+            .checked_add(&n)
+            .filter(|x| x_range.contains(x))
+            .map(|x| (x, self.y).into())
+    }
+
+    pub fn try_left(&self) -> Option<Self> {
+        self.x.checked_sub(&num::one()).map(|x| Self { x, y: self.y })
+    }
+
+    pub fn try_left_by(&self, n: T) -> Option<Self> {
+        self.x.checked_sub(&n).map(|x| Self { x, y: self.y })
+    }
+
+    /// Decrements the `x` coordinate by 1, returning `None` if it goes out of bounds.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use advent_of_code::utils::coords::Coordinates;
+    /// assert_eq!(Coordinates::new(2, 0).try_bounded_left_by(1, 0..), Some(Coordinates::new(1, 0)));
+    /// assert_eq!(Coordinates::new(0, 0).try_bounded_left_by(1, 0..), None);
+    /// ```
+    pub fn try_bounded_left_by<R>(&self, n: T, x_range: R) -> Option<Self> where R: RangeBounds<T> {
+        self.x
+            .checked_sub(&n)
+            .filter(|x| x_range.contains(x))
+            .map(|x| (x, self.y).into())
+    }
+}
+
+/// Utility methods for signed or unsigned coordinates, useful for wrapping around the edges of a grid.
+impl<T> Coordinates<T> where T: Integer + Copy + WrappingAdd + WrappingSub {
+    /// Increments the `y` coordinate by 1, setting it to the lower bound of the range if it exceeds the upper bound.
+    /// Panics if the range is unbounded on either end.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use advent_of_code::utils::coords::Coordinates;
+    /// assert_eq!(Coordinates::new(0, 0).wrapping_up(0..=2), Coordinates::new(0, 1));
+    /// assert_eq!(Coordinates::new(0, 2).wrapping_up(0..=2), Coordinates::new(0, 0));
+    /// assert_eq!(Coordinates::new(0, 2).wrapping_up(-2..=2), Coordinates::new(0, -2));
+    /// ```
+    pub fn wrapping_up<R>(&self, y_range: R) -> Self where R: RangeBounds<T> {
+        let (y_min, y_max) = misc::get_range_min_max(y_range);
+        let y = self.y.wrapping_add(&num::one());
+        let y = Self::get_wrapped(y, y_min, y_max);
+        (self.x, y).into()
+    }
+
+    /// Decrements the `y` coordinate by 1, setting it to the upper bound of the range if it is lower than the lower bound.
+    /// Panics if the range is unbounded on either end.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use advent_of_code::utils::coords::Coordinates;
+    /// assert_eq!(Coordinates::new(0, 2).wrapping_down(0..=2), Coordinates::new(0, 1));
+    /// assert_eq!(Coordinates::new(0, 0).wrapping_down(0..=2), Coordinates::new(0, 2));
+    /// assert_eq!(Coordinates::new(0, -2).wrapping_down(-2..=2), Coordinates::new(0, 2));
+    /// ```
+    pub fn wrapping_down<R>(&self, y_range: R) -> Self where R: RangeBounds<T> {
+        let (y_min, y_max) = misc::get_range_min_max(y_range);
+        let y = self.y.wrapping_sub(&num::one());
+        let y = Self::get_wrapped(y, y_min, y_max);
+        (self.x, y).into()
+    }
+
+    /// Increments the `x` coordinate by 1, setting it to the lower bound of the range if it exceeds the upper bound.
+    /// Panics if the range is unbounded on either end.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use advent_of_code::utils::coords::Coordinates;
+    /// assert_eq!(Coordinates::new(0, 0).wrapping_right(0..=2), Coordinates::new(1, 0));
+    /// assert_eq!(Coordinates::new(2, 0).wrapping_right(0..=2), Coordinates::new(0, 0));
+    /// assert_eq!(Coordinates::new(2, 0).wrapping_right(-2..=2), Coordinates::new(-2, 0));
+    /// ```
+    pub fn wrapping_right<R>(&self, x_range: R) -> Self where R: RangeBounds<T> {
+        let (x_min, x_max) = misc::get_range_min_max(x_range);
+        let x = self.x.wrapping_add(&num::one());
+        let x = Self::get_wrapped(x, x_min, x_max);
+        (x, self.y).into()
+    }
+
+    /// Decrements the `x` coordinate by 1, setting it to the upper bound of the range if it is lower than the lower bound.
+    /// Panics if the range is unbounded on either end.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use advent_of_code::utils::coords::Coordinates;
+    /// assert_eq!(Coordinates::new(2, 0).wrapping_left(0..=2), Coordinates::new(1, 0));
+    /// assert_eq!(Coordinates::new(0, 0).wrapping_left(0..=2), Coordinates::new(2, 0));
+    /// assert_eq!(Coordinates::new(-2, 0).wrapping_left(-2..=2), Coordinates::new(2, 0));
+    /// ```
+    pub fn wrapping_left<R>(&self, x_range: R) -> Self where R: RangeBounds<T> {
+        let (x_min, x_max) = misc::get_range_min_max(x_range);
+        let x = self.x.wrapping_sub(&num::one());
+        let x = Self::get_wrapped(x, x_min, x_max);
+        (x, self.y).into()
+    }
+
+    fn get_wrapped(v: T, v_min: T, v_max: T) -> T {
+        if v > v_max { v_min } else if v < v_min { v_max } else { v }
     }
 }
 
@@ -214,14 +401,38 @@ impl<T> Mul for Coordinates<T> where T: Num + Copy {
     }
 }
 
-impl<T> Display for Coordinates<T> where T: Num + Display {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "({}, {})", self.x, self.y)
+impl<T> From<(T, T)> for Coordinates<T> where T: Num + Copy {
+    fn from((x, y): (T, T)) -> Self {
+        Self::new(x, y)
+    }
+}
+
+impl<T> From<(&T, T)> for Coordinates<T> where T: Num + Copy {
+    fn from((x, y): (&T, T)) -> Self {
+        Self::new(*x, y)
+    }
+}
+
+impl<T> From<(T, &T)> for Coordinates<T> where T: Num + Copy {
+    fn from((x, y): (T, &T)) -> Self {
+        Self::new(x, *y)
+    }
+}
+
+impl<T> From<(&T, &T)> for Coordinates<T> where T: Num + Copy {
+    fn from((x, y): (&T, &T)) -> Self {
+        Self::new(*x, *y)
     }
 }
 
 impl<T> Into<(T, T)> for Coordinates<T> where T: Num {
     fn into(self) -> (T, T) {
         (self.x, self.y)
+    }
+}
+
+impl<T> Display for Coordinates<T> where T: Num + Display {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
     }
 }
