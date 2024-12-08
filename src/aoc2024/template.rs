ade1f498972ba6ded13d365eb929fff7a63cc737
Jonathan Tay <jonathantaymy@gmail.com>
2024-12-08 16:29:52 +0800

    feat(2024): create template

---
+++ src/aoc2024/template.rs
@@ -0,0 +1,16 @@
+use crate::utils::v2::solver;
+
+pub struct Solver;
+
+impl solver::Solver<2024, 1> for Solver {
+    type Part1 = i32;
+    type Part2 = i32;
+
+    fn solve_part_one(&self, input: &str) -> Self::Part1 {
+        0
+    }
+
+    fn solve_part_two(&self, input: &str) -> Self::Part2 {
+        0
+    }
+}
